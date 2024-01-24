926c7faecc1e8f4ae12e7dbd4c4edf4b47eef480
faikyldrm <faik.yldrm@hotmail.com>
2024-01-25 02:07:56 +0300

    Update installAllComponents.sh

--- installAllComponents.sh
+++ installAllComponents.sh
@@ -2,6 +2,7 @@
 echo "Docker Install"
 sudo apt-get install uidmap -y
 curl -fsSL https://get.docker.com/rootless -o get-docker.sh
+export SKIP_IPTABLES=1
 export FORCE_ROOTLESS_INSTALL=1
 sh get-docker.sh
 sudo groupadd docker
